82e56ca95fb6327778c46327986d9247e673ca0a
MIHIR <gohelmihie27@gmail.com>
2023-07-14 17:03:40 +0530

    First Commit

--- server/App.js
+++ server/App.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv')
 const app = express();
 dotenv.config({path:'./config.env'})
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 const cors = require('cors')
 app.use(cors())
 const cookieParser = require('cookie-parser');
